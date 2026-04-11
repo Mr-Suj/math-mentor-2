376baadf4a40b79e513c582b6497f560b9b0e489
Mr-Suj <sujalgowda42@gmail.com>
2026-04-11 07:12:22 +0530

    Refactor LLM configuration and response generation

--- app/config/llm.py
+++ app/config/llm.py
@@ -1,13 +1,14 @@
 import os
-import google.generativeai as genai
+from google import genai
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def generate_response(question: str):
     try:
-        response = model.generate_content(question)
+        response = client.models.generate_content(
+            model="gemini-1.5-flash",
+            contents=question
+        )
         return response.text
     except Exception as e:
         return f"Error: {str(e)}"
